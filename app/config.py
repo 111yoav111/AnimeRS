439f6090c09f3f9488a4b182899e82c6d55a34ff
111yoav111 <2710yoav@gmail.com>
2026-06-19 20:05:23 +0300

    Some fixes, before moving to next part

--- app/config.py
+++ app/config.py
@@ -1,9 +1,7 @@
-
-
-# trace.moe
+# ---------------trace.moe------------------
 TRACE_MOE_BASE_URL = "https://api.trace.moe"  # API path
 TRACE_MOE_CUT_BORDERS = True  # cut borders of img
 TRACE_MOE_TIMER = 8.0  # max seconds of waiting for responde
 
 # Confidence gate
-CONFIDENT_THRESHOLD = 0.95  # if trace.moe have 95% simillarty - dont ask other API
+CONFIDENT_THRESHOLD = 95  # if trace.moe have x>95% simillarty - dont ask other API
